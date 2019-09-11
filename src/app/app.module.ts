fc5f69ad318a2da252f5fbab9f386847e65c1f0c
Milan Vukovic <mildzan@gmail.com>
2019-09-11 21:02:39 +0200

    search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { ParentComponent } from './parent/parent.component';
@@ -9,8 +10,8 @@ import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [AppComponent, ParentComponent, ChildComponent, DetailsComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -10,12 +10,25 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class ParentComponent implements OnInit {
   data: Person[];
+  filteredPersons: Person[]
   personToDisplay: Person;
   previousPerson: Person = null;
   private arrayIndex = 1;
   numOfPersons: number;
   toDisplay: boolean = true;
   visitPerson: number;
+
+  private _searchTerm: string
+  get searchTerm() {
+    return this._searchTerm
+  }
+  set searchTerm(value: string) {
+    this._searchTerm = value;
+    this.filteredPersons = this.filterPersons(value)
+  }
+  filterPersons(searchString: string) {
+    return this.data.filter(person => person.firstname.toLowerCase().indexOf(searchString.toLowerCase()) !== -1)
+  }
 
   constructor(
     private dataService: DataService,
@@ -25,11 +38,18 @@ export class ParentComponent implements OnInit {
 
   ngOnInit() {
     this.dataService.getData().subscribe(data => {
-      this.data = data
+      this.data = data;
+      this.filteredPersons = this.data;
       this.personToDisplay = this.data[0];
       this.numOfPersons = this.data.length;
     });
     this.visitPerson = +this.route.snapshot.paramMap.get('id')
+    if (this.route.snapshot.queryParamMap.has('searchTerm')) {
+      this.searchTerm = this.route.snapshot.queryParamMap.get('searchTerm')
+    } else {
+      this.filteredPersons = this.data;
+    }
+
   }
 
   nextPerson(): void {
@@ -46,6 +66,7 @@ export class ParentComponent implements OnInit {
   }
 
   onClick(id: number) {
-    this.router.navigate(['/details', id]);
+    this.router.navigate(['/details', id],
+      { queryParams: { 'searchTerm': this.searchTerm } })
   }
 }
